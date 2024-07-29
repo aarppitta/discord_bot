d704ae07ed70d8d288fa6b8e2e61f6148a0b780c
aarrppiittaa <arpitachaudhari26@gmail.com>
2024-07-29 20:15:40 +0100

    add command and index files

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        // GatewayIntentBits.MessageContent,
+    ]
+ });
+
+client.on("messageCreate", message => {
+    if(message.author.bot) return;
+        message.reply("Hello I am the bot");
+    });
+
+client.on("interactionCreate", interaction => {
+    console.log(interaction);
+    interaction.reply("Pong!");
+});
+
+
+client.login('token')
